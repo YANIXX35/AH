bdd20dce4c8b5b0ebf07cf9d1c32d4329a8ed84c
YANIXX35 <yaoyanissekyliane@gmail.com>
2026-08-06 11:52:52 +0000

    Remove forced password change for commercial/accountant-created clients
    Clients created via the Ajouter un client flow (Commercial/Comptable)
    were redirected to /profile with a blocking warning until they changed
    their temporary shared password, before reaching any other section.
    
    Removes the EnsurePasswordHasBeenChanged middleware and its dedicated
    test.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,6 @@ use App\Http\Middleware\EnsureAccountant;
 use App\Http\Middleware\EnsureAccountNotSuspended;
 use App\Http\Middleware\EnsureCommercial;
 use App\Http\Middleware\EnsureModulePermission;
-use App\Http\Middleware\EnsurePasswordHasBeenChanged;
 use App\Http\Middleware\EnsurePlatformAdmin;
 use App\Http\Middleware\EnsurePremiumAccountingAccess;
 use App\Http\Middleware\LogMenuNavigation;
@@ -43,7 +42,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->appendToGroup('web', SetAppLocale::class);
         $middleware->appendToGroup('web', EnsureAccountNotSuspended::class);
-        $middleware->appendToGroup('web', EnsurePasswordHasBeenChanged::class);
         $middleware->appendToGroup('web', SanitizeClientWorkspaceSession::class);
         $middleware->appendToGroup('web', EnforceAccountantPortalPolicy::class);
         $middleware->appendToGroup('web', LogMenuNavigation::class);
